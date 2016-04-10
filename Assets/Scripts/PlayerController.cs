ef7aa11090a3dc5db7d60759a0445b4f5e7c5f0f
Justin Krasner-Karpen <justinkk@DN51u4iu.SUNet>
2016-04-09 17:02:12 -0700

    Added levels of stats

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,42 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	//"Constants" that can be changed in the editor
-	public int MAX_SPEED = 1;
-	public float ACCELERATION = 4.0f;
-	public float TRACTION = 2.0f;
+	public int MAX_SPEED;
+	public float ACCELERATION;
+	public float TRACTION;
 	public float INPUT_THRESHOLD = 0.3f; //Button pressed if axis > this threshold
+	public float PERCENT_PER_LEVEL = 0.1f; //How much a stat changes per stat level
 	private static float ONE_OVER_ROOT_TWO = 1.0f / Mathf.Sqrt(2.0f);
 
 	//Instance variables and variables that behave like them
 	private Rigidbody2D body;
 	private Animator animator;
+	private int speedLevel;
+	private int accelerationLevel;
+	private int tractionLevel;
 
 	//Returns whether the axis parameter is big enough to represent a button press
 	bool Pressed(float axis) {
 		return Mathf.Abs(axis) > INPUT_THRESHOLD;
 	}
 
+	//Gives current value of a stat, given base value and current level
+	private float ComputeStat(float baseStat, int statLevel) {
+		if (statLevel > 0) 
+			return baseStat * (1.0f + PERCENT_PER_LEVEL * statLevel);
+		else if (statLevel < 0)
+			return baseStat * (Mathf.Pow(1.0f - PERCENT_PER_LEVEL, -statLevel));
+		else
+			return baseStat;
+	}
+
 	//Called on the object's creation
 	void Start() {
 		body = gameObject.GetComponent<Rigidbody2D>();
 		animator = gameObject.GetComponent<Animator>();
+		speedLevel = 0;
+		accelerationLevel = 0;
+		tractionLevel = 0;
 	}
 
 	//Called once per physics step
@@ -33,7 +50,7 @@ public class PlayerController : MonoBehaviour {
 															  // controllers
 		Vector2 force = Vector2.zero;
 		if (verticalInput > INPUT_THRESHOLD) {
-			force += Vector2.up;
+			force += Vector2.up;			   //TODO: consider analog input?
 			animator.SetBool("GoingUp", true); //TODO: this probably belongs in Update() instead of FixedUpdate()
 		} else if (verticalInput < -INPUT_THRESHOLD) { // but it's a terrible animation anyway
 			force += Vector2.down;
@@ -57,23 +74,25 @@ public class PlayerController : MonoBehaviour {
 		//Apply force
 		if (force == Vector2.zero) {
 			//Apply traction
-			body.drag = TRACTION;
+			body.drag = ComputeStat(TRACTION, tractionLevel);
 		} else {
 			body.drag = 0;
-			force *= ACCELERATION;
+			force *= ComputeStat(ACCELERATION, accelerationLevel);
 			body.AddForce(force);
 		}
 
 		//Handle top speed
 		Vector2 currVelocity = body.velocity;
-		if (currVelocity.magnitude > MAX_SPEED) { 
+		float currMaxSpeed = ComputeStat(MAX_SPEED, speedLevel);
+		if (currVelocity.magnitude > currMaxSpeed) { 
 			currVelocity.Normalize();
-			body.velocity = currVelocity * MAX_SPEED;
+			body.velocity = currVelocity * currMaxSpeed;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update() {
-	
+		//int val = (int)Mathf.Floor(Random.Range(-10.0f, 10.0f));
+		//Debug.Log("level: " + val + ", stat computed: " + ComputeStat(MAX_SPEED, val));
 	}
 }
