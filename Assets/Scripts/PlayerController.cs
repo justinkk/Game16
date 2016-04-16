8f4b2c4f5c51f65b9d883c9c8124ded169445cd9
Justin Krasner-Karpen <justinkk@DN0a22634d.SUNet>
2016-04-16 15:08:49 -0700

    added 1st version of boost button!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	//Constants
-	private static readonly float[] DEFAULT_STATS = {5, 10, 1.2f}; //Default base stats
-	//public int MAX_SPEED;
-	//public float ACCELERATION;
-	//public float TRACTION;
+	private static readonly float[] DEFAULT_STATS = {5, 10, 1.2f, 30}; //Default base stats
 	public static float INPUT_THRESHOLD = 0.1f; //Button pressed if axis > this threshold
-	//public float PERCENT_PER_LEVEL = 0.1f; //How much a stat changes per stat level
-	private static readonly float[] BOOST_PER_STAT = {0.05f, 0.1f, 0.1f}; //How much each stat changes per stat level
+	private static readonly float[] BOOST_PER_STAT = {0.05f, 0.1f, 0.1f, 0.1f}; //How much each stat changes per stat level
 	private static readonly float ONE_OVER_ROOT_TWO = 1.0f / Mathf.Sqrt(2.0f);
+
+	//Brakes stat computes drag amount
+	//Multiply by this to get %charged per second
+	public const float BRAKES_TO_CHARGE_SPEED = 0.8f;
 
 	//Instance variables
 	private Rigidbody2D body;
@@ -20,14 +20,22 @@ public class PlayerController : MonoBehaviour {
 	private int[] statLevels; 
 	//The time at which player started charging/boosting
 	private float chargeStart; 
-	private float boostStart;
+	//private float boostStart;
 	//For keeping track of boost effect; default to false
 	private bool charging;
-	private bool boosting;
-	//Boost stat for percent per second how quickly your boost runs out
+	//private bool boosting;
+	
+	/**
+	 * Returns what percent charged you are
+	 * Maximum of 100%
+	 * Only call when you're currently charging
+	 */
+	private float ChargePercent() {
+		return Mathf.Min((Time.time - chargeStart) * ComputeStat(StatConstants.BRAKES), 1.0f);
+	}
 
 	//Returns whether the axis parameter is big enough to represent a button press
-	bool Pressed(float axis) {
+	private bool Pressed(float axis) {
 		return Mathf.Abs(axis) > INPUT_THRESHOLD;
 	}
 
@@ -93,7 +101,7 @@ public class PlayerController : MonoBehaviour {
 		
 		//Apply force
 		if (force == Vector2.zero) {
-			//Apply brakes
+			//Apply brakes with linear drag
 			body.drag = ComputeStat(StatConstants.BRAKES);
 		} else {
 			body.drag = 0;
@@ -101,35 +109,53 @@ public class PlayerController : MonoBehaviour {
 			body.AddForce(force);
 		}
 
-		//Handle top speed
+		//Handle top speed with quadratic drag
 		Vector2 currVelocity = body.velocity;
-		float currMaxSpeed = ComputeStat(StatConstants.SPEED);
-		//if (currVelocity.magnitude > currMaxSpeed) {
 		float magnitude = currVelocity.magnitude;
 		currVelocity.Normalize();
 		body.AddForce(-currVelocity * magnitude * magnitude * DragCoefficient());
-			//body.AddForce(-currVelocity * magnitude * 1.15f);
-			/*
-			currVelocity.Normalize();
-			body.velocity = currVelocity * currMaxSpeed;
-			*/
-		//}
 	}
 	
 	// Update is called once per frame
 	void Update() {
-		Debug.Log(body.velocity);
+		Debug.Log(charging);
 		//Record when you started charging boost
 		if (Input.GetKeyDown("space")) {
 			charging = true;
-			boosting = false;
+			//boosting = false;
 			chargeStart = Time.time;
 		}
-		//Record when you started boosting
+		//Apply a boost
 		if (Input.GetKeyUp("space")) {
 			charging = false;
-			boosting = true;
-			boostStart = Time.time;
+			//boosting = true;
+			//boostStart = Time.time;
+			//Figure out which direction to travel in
+			float verticalInput = Input.GetAxisRaw("Vertical"); //TODO: Replace Input.GetAxisRaw with something better,
+			float horizontalInput = Input.GetAxisRaw("Horizontal");  // and something that could work with multiple
+																  // controllers
+			Vector2 force = Vector2.zero;
+			if (verticalInput > INPUT_THRESHOLD) {
+				force += Vector2.up;			   //TODO: consider analog input?
+				animator.SetBool("GoingUp", true); //TODO: this probably belongs in Update() instead of FixedUpdate()
+			} else if (verticalInput < -INPUT_THRESHOLD) { // but it's a terrible animation anyway
+				force += Vector2.down;
+				animator.SetBool("GoingUp", false);
+			}
+			if (horizontalInput > INPUT_THRESHOLD) {
+				force += Vector2.right;
+				animator.SetBool("GoingRight", true);
+			} else if (horizontalInput < -INPUT_THRESHOLD) {
+				force += Vector2.left;
+				animator.SetBool("GoingRight", false);
+			}
+
+			if (Pressed(verticalInput) && Pressed(horizontalInput)) {
+				//Diagonal: normalize the movement speed
+				force *= ONE_OVER_ROOT_TWO;	
+			} 
+
+			body.AddForce(force * ChargePercent() * ComputeStat(StatConstants.BOOST), ForceMode2D.Impulse);
 		}
 	}
 
--- Assets/Scripts/StatConstants.cs
+++ Assets/Scripts/StatConstants.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 
 public static class StatConstants {
-	public const int NUM_STATS = 3;
+	public const int NUM_STATS = 4;
 
 	public const int SPEED = 0;
 	public const int ACCELERATION = 1;
 	public const int BRAKES = 2;
+	public const int BOOST = 3;
 }
