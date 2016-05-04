53c95abed6ce055fca6384d8796057ddf9ef5305
Lingtong Sun <lingtongsun@gmail.com>
2016-05-04 10:46:29 -0700

    terrain stuff

---
+++ Assets/Scripts/TerrainController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		gameObject.transform.localPosition = new Vector3 (0, 0, 0);
+		gameObject.transform.localScale = new Vector3 (2f, 2f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
