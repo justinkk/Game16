ab4a1d80af14e348637b695452cb7cf9408c05a6
Jonathan Noyola <jnoyola@stanford.edu>
2016-05-08 19:43:58 -0700

    added water and zoomed out camera (MainScene changes)

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+    public float x_speed = 1;
+    public float y_speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 offset = new Vector2(Time.time * x_speed, Time.time * y_speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
