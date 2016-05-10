6e3f94e61550de9cf7e2a5fab56bb3f5508ad438
Jonathan Noyola <jnoyola@stanford.edu>
2016-05-10 12:56:40 -0700

    water now flows back and forth

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -3,17 +3,35 @@ using System.Collections;
 
 public class Scroll : MonoBehaviour {
 
-    public float x_speed = 1;
-    public float y_speed = 1;
+    public float x_max_speed = 0.01f;
+    public float y_max_speed = 0.01f;
+    public float x_acc = 0.006f;
+    public float y_acc = 0.006f;
 
-	// Use this for initialization
-	void Start () {
+    private float x_speed = 0;
+    private float y_speed = 0;
+    private float x_dir = 1;
+    private float y_dir = 1;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 offset = new Vector2(Time.time * x_speed, Time.time * y_speed);
+        x_speed += Time.deltaTime * x_acc * x_dir;
+        y_speed += Time.deltaTime * y_acc * y_dir;
+        if ((x_speed > x_max_speed && x_dir > 0) || (x_speed < -x_max_speed && x_dir < 0)) {
+            x_dir *= -1;
+        }
+        if ((y_speed > y_max_speed && y_dir > 0) || (y_speed < -y_max_speed && y_dir < 0)) {
+            y_dir *= -1;
+        }
+
+        Vector2 offset = GetComponent<Renderer>().material.mainTextureOffset;
+        offset.x += Time.deltaTime * x_speed;
+        offset.y += Time.deltaTime * y_speed;
         GetComponent<Renderer>().material.mainTextureOffset = offset;
 	}
 }
