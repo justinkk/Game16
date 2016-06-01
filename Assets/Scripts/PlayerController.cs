dbbb1625d47af62aa65a1f277b34360322e92ef6
Gus Liu <gusliu@rescomp-15-322798.stanford.edu>
2016-06-01 13:28:19 -0700

    Fixed typo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,7 +39,7 @@ public class PlayerController : MonoBehaviour {
 	//private Animator animator;
 	private float[] baseStats; //Instance variable in case we want to allow multiple vehicles
 	private int[] statLevels;
-	private mostRecentTimeStatStolen = 0;
+	private int mostRecentTimeStatStolen = 0;
 
 	private bool charging = false;
 	private float chargeStart;
